3c425be6688fc3e6a1331424149bd46a2c5646d7
Salam Rahal-Arabi <strarabi@gmail.com>
2023-10-26 22:53:30 -0700

    Some work on recipe interface and recipe modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,29 @@
-import RecipeCard from './components/RecipeCard';
+import RecipeCard, { Recipe } from './components/RecipeCard';
 import Navbar from './components/Navbar';
+import React, { useState } from 'react'
 
 function App() {
+  const [recipeCards, setRecipeCards] = useState<JSX.Element[]>([]);
+  for (let i = 0; i < 8 ; ++i) {
+    const r: Recipe = {
+      recipe_name: "Rice and Beans", 
+      recipe_description: "Healthy, efficient, and budget-friendly recipe that is enjoyed by college students worldwide.",
+      recipe_body: "recipe body goes here",
+      cost_per_serving: "$1",
+      time_to_prepare: "5",
+      perishability_of_ingredients: "0",
+      num_dishes_required: "1",
+      difficulty: "0.1",
+      recipe_image: "http://placekitten.com/400/250"
+      }
+
+    recipeCards.push(<RecipeCard recipe={r} />)
+  }
   return (
     <div className='app-container'>
       <Navbar/>
       <div className="gap-10 pt-10 flex flex-wrap justify-center">
-        <RecipeCard recipe_name='rice and beans' recipe_description='Affordable, healthy, efficient, and budget-friendly recipe that is enjoyed by college students worldwide.' />
-        <RecipeCard recipe_name='rice and beans' recipe_description='Affordable, healthy, efficient, and budget-friendly recipe that is enjoyed by college students worldwide.' />
-        <RecipeCard recipe_name='rice and beans' recipe_description='Affordable, healthy, efficient, and budget-friendly recipe that is enjoyed by college students worldwide.' />
-        <RecipeCard recipe_name='rice and beans' recipe_description='Affordable, healthy, efficient, and budget-friendly recipe that is enjoyed by college students worldwide.' />
-        <RecipeCard recipe_name='rice and beans' recipe_description='Affordable, healthy, efficient, and budget-friendly recipe that is enjoyed by college students worldwide.' />
-        <RecipeCard recipe_name='rice and beans' recipe_description='Affordable, healthy, efficient, and budget-friendly recipe that is enjoyed by college students worldwide.' />
-        <RecipeCard recipe_name='rice and beans' recipe_description='Affordable, healthy, efficient, and budget-friendly recipe that is enjoyed by college students worldwide.' />
-        <RecipeCard recipe_name='rice and beans' recipe_description='Affordable, healthy, efficient, and budget-friendly recipe that is enjoyed by college students worldwide.' />
-        <RecipeCard recipe_name='rice and beans' recipe_description='Affordable, healthy, efficient, and budget-friendly recipe that is enjoyed by college students worldwide.' />
+        {recipeCards}
       </div>
     </div>
   );
--- client/src/components/RecipeCard.tsx
+++ client/src/components/RecipeCard.tsx
@@ -1,17 +1,47 @@
-export interface Props {
+export interface Recipe {
     recipe_name: string,
     recipe_description: string,
+    recipe_body: string,
+    recipe_image: string,
+    cost_per_serving: string,
+    time_to_prepare: string, // seconds
+    perishability_of_ingredients: string, // normalized between 0 and 1 (1 = all ingredients are non-perishable)
+    num_dishes_required: string,
+    difficulty: string // normalized between 0 and 1 (1 = maximum difficulty)
+}
+
+
+export interface Props {
+    recipe: Recipe
 }
 
 function RecipeCard(props: Props): JSX.Element {
     return (
     <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="http://placekitten.com/400/250" alt="Shoes" /></figure>
+        <figure><img src={props.recipe.recipe_image} alt="Recipe Picture" /></figure>
         <div className="card-body">
-            <h2 className="card-title">{props.recipe_name}</h2>
-            <p>{props.recipe_description}</p>
+            <h2 className="card-title">{props.recipe.recipe_name}</h2>
+            <p>{props.recipe.recipe_description}</p>
             <div className="card-actions justify-end">
-            <button className="btn">Show Me</button>
+            <button className="btn" onClick={()=>(document.getElementById('recipe_info') as HTMLDialogElement).showModal()}>Show Me</button>
+
+            <dialog id="recipe_info" className="modal"> 
+                <div className="modal-box">
+                    <h1 className="font-bold text-center text-xl">{props.recipe.recipe_name}</h1>
+                    <h1 className="font-bold text-lg">Recipe Info</h1>
+                    <p>Cost Per Serving: {props.recipe.cost_per_serving} </p>
+                    <p>Time to Prepare: {props.recipe.time_to_prepare} </p>
+                    <p>Perishability of Ingredients: {props.recipe.perishability_of_ingredients} </p>
+                    <p>Number of Dishes Required: {props.recipe.num_dishes_required} </p>
+                    <p>Meal Difficulty: {props.recipe.difficulty} </p>
+                    <h1 className="font-bold text-lg">Recipe</h1>
+                    <p>{props.recipe.recipe_body}</p>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+
             </div>
         </div>
     </div>
